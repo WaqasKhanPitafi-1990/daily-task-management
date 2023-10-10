9ae1f6b3161607e469797825f2c7e628b8173599
usama2333 <usama.wizz@gmail.com>
2023-10-10 15:06:32 +0500

    drag and drop task from one column to another

--- components/Board.tsx
+++ components/Board.tsx
@@ -9,9 +9,10 @@ const Board = () => {
     // const getBoard = useBoardStore((state) => state.getBoard);
     // const board = useBoardStore((state) => state.board);
 
-    const [board, getBoard] = useBoardStore((state) => [
+    const [board, getBoard, setBoardState] = useBoardStore((state) => [
         state.board,
         state.getBoard,
+        state.setBoardState,
     ])
     
   useEffect(() => {
@@ -22,7 +23,80 @@ const Board = () => {
   console.log(board,'board....')
 
   const handleOnDragEnd = (result: DropResult) => {
-    
+     const {destination, source, type} = result;
+    //  console.log(destination,'des.....')
+    //  console.log(source,'sour......')
+    //  console.log(type,'type..........')
+
+    if(!destination) return;
+
+     //Handle column drag
+     if(type === 'column') {
+        const entries = Array.from(board.columns.entries());
+        const [removed] = entries.splice(source.index, 1);
+        entries.splice(destination.index, 0, removed);
+        const rearrangedColumns = new Map(entries);
+        setBoardState({
+            ...board,
+            columns: rearrangedColumns,
+        })
+     }
+
+     //This step is needed as the indexes are stored as numbers 0,1,2,3 instead of id's 
+     const columns = Array.from(board.columns);
+     const startColIndex = columns[Number(source.droppableId)];
+     const finishColIndex = columns[Number(destination.droppableId)];
+
+     const startCol: Column = {
+        id: startColIndex[0],
+        todos: startColIndex[1].todos,
+     }
+
+     const finishCol: Column = {
+        id: finishColIndex[0],
+        todos: finishColIndex[1].todos,
+     }
+
+     if(!startCol || !finishCol) return;
+
+     if(source.index === destination.index && startCol === finishCol) return;
+
+     const newTodos = startCol.todos;
+     const [todoMoved] = newTodos.splice(source.index, 1);
+
+     if(startCol.id === finishCol.id) {
+
+        //same column task drag
+        newTodos.splice(destination.index, 0, todoMoved);
+        const newCol = {
+            id: startCol.id,
+            todos: newTodos,
+        }
+        const newColumns = new Map(board.columns);
+        newColumns.set(startCol.id, newCol);
+
+        setBoardState({...board, columns: newColumns})
+     }
+     else {
+          // dragging to another column
+          const finishTodos = Array.from(finishCol.todos);
+          finishTodos.splice(destination.index, 0, todoMoved);
+
+          const newColumns = new Map(board.columns);
+          const newCol = {
+            id: startCol.id,
+            todos: newTodos,
+          }
+
+          newColumns.set(startCol.id, newCol);
+          newColumns.set(finishCol.id,{
+            id: finishCol.id,
+            todos: finishTodos,
+          })
+
+          setBoardState({...board, columns:newColumns})
+     }
+
   }
 
   return (
--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { XCircleIcon } from '@heroicons/react/24/solid';
 import React from 'react'
 import { DraggableProvidedDragHandleProps,DraggableProvidedDraggableProps } from 'react-beautiful-dnd'
 
@@ -21,7 +22,15 @@ const TodoCard = ({todo, index, id, innerRef, draggableProps, dragHandleProps} :
      {...dragHandleProps}
      ref={innerRef}
     >
-      <h1>Hello</h1>
+      
+      <div className='flex justify-between items-center p-5'>
+        <p>{todo.title}</p>
+        <button className='text-red-500 hover:text-red-600'>
+            <XCircleIcon className='ml-5 h-8 w-8'/>
+        </button>
+
+      </div>
+
     </div>
   )
 }
